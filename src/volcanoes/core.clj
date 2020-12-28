ae0af135a66a84372961254b4444efbe8b59ffb9
Ike Olumese <IkeOlumese@latacora.com>
2020-12-28 17:29:54 -0500

    Read in csv file and turned data into a vector of maps

--- src/volcanoes/core.clj
+++ src/volcanoes/core.clj
@@ -1,6 +1,17 @@
-(ns volcanoes.core)
+(ns volcanoes.core
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io  :as io]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def csv-lines
+  (with-open [csv (io/reader "/home/user/volcanoes/resources/GVP_Volcano_List_Holocene.csv")]
+    (doall
+      (csv/read-csv csv)))
+  )
+(def volcano-records
+  (let [csv-lines     (rest csv-lines)
+        header-line   (first csv-lines)
+        volcano-lines (rest csv-lines)]
+    (map (fn [volcano-line]
+           (zipmap header-line volcano-line))
+         volcano-lines))
+  )
